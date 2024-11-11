2158d5b59acada499167814084d247cf4dee9660
ShilpiSoni18 <shilpisoni7899@gmail.com>
2024-11-11 22:57:59 +0000

    OTP file modified

--- otp.py
+++ otp.py
@@ -1,6 +1,5 @@
-# print("hello")
 # Create first python project to generate 6 digit random otp
 import random;
 
-otp=int(random.random()*1000000);
-print("Your otp is:\t",otp);
+target_otp=int(random.random()*1000000);
+print("Your otp is:\t",target_otp);
